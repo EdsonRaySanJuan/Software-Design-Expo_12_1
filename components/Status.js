e44a126bfbb7235508d733cc88630bade5bfbc25
EdsonRaySanJuan <qeresanjuan@tip.edu.ph>
2026-04-01 10:54:19 +0800

    Done HOA 12.1

---
+++ components/Status.js
@@ -0,0 +1,88 @@
+// Status.js
+import React from 'react';
+import Constants from 'expo-constants';
+import { Platform, StatusBar, StyleSheet, Text, View } from 'react-native';
+import NetInfo from '@react-native-community/netinfo';
+
+export default class Status extends React.Component {
+  state = {
+    info: null,
+  };
+
+  async componentDidMount() {
+    const state = await NetInfo.fetch();
+    this.setState({ info: state.type });
+
+    this.subscription = NetInfo.addEventListener(state => {
+      this.handleConnectivityChange(state.type);
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.subscription) {
+      this.subscription();
+    }
+  }
+
+  handleConnectivityChange = (connectionType) => {
+    this.setState({ info: connectionType });
+  };
+
+  render() {
+    const { info } = this.state;
+    const isConnected = info !== 'none' && info !== 'unknown';
+    const backgroundColor = isConnected ? 'white' : 'red';
+
+    const statusBar = (
+      <StatusBar
+        backgroundColor={backgroundColor}
+        barStyle={isConnected ? 'dark-content' : 'light-content'}
+        animated={false}
+      />
+    );
+
+    const messageContainer = (
+      <View style={styles.messageContainer} pointerEvents={'none'}>
+        {statusBar}
+        {!isConnected && (
+          <View style={styles.bubble}>
+            <Text style={styles.text}>No network connection</Text>
+          </View>
+        )}
+      </View>
+    );
+
+    return (
+      <View style={[styles.status, { backgroundColor }]}>
+        {messageContainer}
+      </View>
+    );
+  }
+}
+
+const statusHeight = Constants.statusBarHeight;
+
+const styles = StyleSheet.create({
+  status: {
+    zIndex: 1,
+    height: statusHeight,
+  },
+  messageContainer: {
+    zIndex: 1,
+    position: 'absolute',
+    top: statusHeight + 20,
+    right: 0,
+    left: 0,
+    height: 80,
+    alignItems: 'center',
+  },
+  bubble: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 20,
+    backgroundColor: 'red',
+  },
+  text: {
+    color: 'white',
+  },
+});
